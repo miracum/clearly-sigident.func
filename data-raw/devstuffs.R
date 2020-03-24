4dfaccaf8a0bb79da09fd864a024795d2300e91a
Kapsner <lorenz.kapsner@uk-erlangen.de>
2020-03-24 16:24:14 +0100

    updated version to v0.0.2

--- data-raw/devstuffs.R
+++ data-raw/devstuffs.R
@@ -19,7 +19,7 @@ my_desc$del("Maintainer")
 # Vignette Builder
 my_desc$set("VignetteBuilder" = "knitr")
 # Set the version
-my_desc$set_version("0.0.1")
+my_desc$set_version("0.0.2")
 # The title of your package
 my_desc$set(Title = "Sigident Functional Analysis")
 # The description of your package
@@ -72,7 +72,7 @@ usethis::use_package("limma", type="Imports")
 
 
 # Development package
-mytag <- "v0.0.2"
+mytag <- "v0.0.3"
 devtools::install_git(url = "https://gitlab.miracum.org/clearly/sigident.preproc", ref = mytag, upgrade = "always")
 # https://cran.r-project.org/web/packages/devtools/vignettes/dependencies.html
 desc::desc_set_remotes(c(
@@ -99,6 +99,7 @@ usethis::use_build_ignore("geodata")
 usethis::use_build_ignore("plots")
 usethis::use_build_ignore("csv")
 usethis::use_build_ignore("ci")
+usethis::use_build_ignore(".vscode")
 
 # gitignore
 usethis::use_git_ignore("/*")
@@ -123,6 +124,7 @@ usethis::use_git_ignore("/.Rhistory")
 usethis::use_git_ignore("/*.Rproj")
 usethis::use_git_ignore("/.Rproj*")
 usethis::use_git_ignore("/.RData")
+usethis::use_git_ignore("/.vscode")
 
 # code coverage
 #covr::package_coverage()
